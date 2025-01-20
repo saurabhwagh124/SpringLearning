6e48552a0798fefc962230191b7d8600e7b03189
saurabhwagh124 <saurabhwagh124@gmail.com>
2025-01-20 16:45:51 +0530

    Springboot ecommerce backend project done

---
+++ SpringEcommerce/src/main/java/com/saurabh/SpringEcommerce/controller/ProductController.java
@@ -0,0 +1,83 @@
+package com.saurabh.SpringEcommerce.controller;
+
+import com.saurabh.SpringEcommerce.model.Product;
+import com.saurabh.SpringEcommerce.service.ProductService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts() {
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable int productId) {
+        Product product = productService.getProductById(productId);
+        if (product.getId() > 0) {
+            return new ResponseEntity<>(productService.getProductById(productId), HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile image) {
+        Product savedProduct = null;
+        try {
+            savedProduct = productService.addOrUpdateProduct(product, image);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping("product/{id}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int id) {
+        Product product = productService.getProductById(id);
+        return new ResponseEntity<>(product.getImageData(), HttpStatus.OK);
+    }
+
+    @PutMapping("product/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestBody Product product, @RequestPart MultipartFile image) {
+        Product updateProduct = null;
+        try{
+            updateProduct = productService.addOrUpdateProduct(product, image);
+            return new ResponseEntity<>("updated", HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product = productService.getProductById(id);
+        if(product != null){
+            productService.deleteProduct(id);
+            return new ResponseEntity<>("deleted", HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("products/search")
+    public ResponseEntity<List<Product>> searchProduct(@RequestParam String keyword){
+        List<Product> products = productService.searchProducts(keyword);
+        System.out.println("searching with "+keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+}
